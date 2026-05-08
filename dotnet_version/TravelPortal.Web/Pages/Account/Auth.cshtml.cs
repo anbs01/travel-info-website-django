9da0cc898e27a29576c1f55d0c65777957ee30a7
lqh <anbs01@sina.com>
2026-05-08 19:12:03 +0800

    feat(ui): 实现二维码悬停效果并添加静态会员认证提示页

---
+++ dotnet_version/TravelPortal.Web/Pages/Account/Auth.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelPortal.Web.Pages.Account
+{
+    public class AuthModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
